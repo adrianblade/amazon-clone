d70f42836cc5e47cb8a5561b22593f8cd51b8208
Adrian Catalan <adrianblade6@gmail.com>
2020-10-18 19:43:06 +0200

    Product component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import './App.css';
 import Header from './Header'
+import Home from './Home'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 
 function App() {
   return (
+    <Router>
     <div className="app">
-      <Header/>
-      {/* HOME */}
-      {/* HEADER */}
+      <Switch>
+      <Route path="/checkout">
+          <Header/>
+          <p>Im a checkout page</p>
+        </Route>
+        <Route path="/">
+          <Header/>
+          <Home/>
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
