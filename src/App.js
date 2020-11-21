5fca77a5fc10104c71521743426e099d2f4a70a4
Adrian Catalan <adrianblade6@gmail.com>
2020-11-21 18:39:11 +0100

    Payment component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, { useEffect } from "react";
-import './App.css';
-import Header from './Header'
-import Home from './Home'
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Checkout from './Checkout'
+import Checkout from "./Checkout";
 import Login from "./Login";
+import Payment from "./Payment";
+import Orders from "./Order";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
+const promise = loadStripe(
+  "pk_test_51HPvU9DFg5koCdLGJJbNo60QAU99BejacsvnKvT8xnCu1wFLCuQP3WBArscK3RvSQmSIB3N0Pbsc7TtbQiJ1vaOi00X9sIbazL"
+);
 
 function App() {
   const [{}, dispatch] = useStateValue();
 
   useEffect(() => {
     // will only run once when the app component loads...
+
     auth.onAuthStateChanged((authUser) => {
       console.log("THE USER IS >>> ", authUser);
 
@@ -36,21 +44,31 @@ function App() {
 
   return (
     <Router>
-    <div className="app">
-      <Switch>
-        <Route path="/login">
-          <Login />
+      <div className="app">
+        <Switch>
+          <Route path="/orders">
+            <Header />
+            <Orders />
           </Route>
-        <Route path="/checkout">
-          <Header/>
-          <Checkout/>
-        </Route>
-        <Route path="/">
-          <Header/>
-          <Home/>
-        </Route>
-      </Switch>
-    </div>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
